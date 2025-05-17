d1a9634433d5271d8481bbe1c6348e8efa147e1b
Harish Prabhu <111897267+Harishprabhu30@users.noreply.github.com>
2025-05-18 01:38:11 +0300

    calibration Added

--- IMU-DataAcquisition-using-RTOS/main/main.c
+++ IMU-DataAcquisition-using-RTOS/main/main.c
@@ -44,26 +44,73 @@ esp_err_t load_or_store_bias()
 
     // Load or store each accelerometer bias
     for (int i = 0; i < NUM_AXIS; i++) {
-        err = nvs_get_blob(nvs_handle, accel_keys[i], &accel_bias[i], sizeof(float));
+        size_t length = sizeof(float);
+        err = nvs_get_blob(nvs_handle, accel_keys[i], &accel_bias[i], &length);
         if (err != ESP_OK) {
             accel_bias[i] = accel_bias_deafults[i]; // Use default value
-            nvs_get_blob(nvs_handle, accel_keys[i], &accel_bias[i], sizeof(float));
-            any_missing = true; // Mark that a value was missing
+            nvs_get_blob(nvs_handle, accel_keys[i], &accel_bias[i], &length);
+            any_missing = true; // Marks if a value is missing in NVS
         }
     }
 
     // Load or store each gyroscope bias
     for (int i = 0; i < NUM_AXIS; i++) {
-        err = nvs_get_blob(nvs_handle, gyro_keys[i], &gyro_bias[i], sizeof(float));
+        size_t length = sizeof(float);
+        err = nvs_get_blob(nvs_handle, gyro_keys[i], &gyro_bias[i], &length);
         if (err != ESP_OK) {
             gyro_bias[i] = gyro_bias_defaults[i]; // Use default value
-            nvs_get_blob(nvs_handle, gyro_keys[i], &gyro_bias[i], sizeof(float));
+            nvs_get_blob(nvs_handle, gyro_keys[i], &gyro_bias[i], &length);
             any_missing = true; // Mark that a value was missing
         }
     }
+
+    // If any value was missing,
+    if (any_missing) {
+        err = nvs_commit(nvs_handle); // Commit changes
+        if (err != ESP_OK) {
+            nvs_close(nvs_handle);
+            return err; // Return error if commit fails
+        }
+    }
+
+    // Close NVS
+    nvs_close(nvs_handle);
+    return ESP_OK; // Return success
 }
 
 void app_main(void)
 {
+    // Configure LED
+    gpio_reset_pin(LED_GPIO); // Reset the GPIO pin
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT); // Set the GPIO pin as output
 
+    // blink 3 times to indicate start
+    blink_led(3, 200);
+
+    // Initialize NVS
+    esp_err_t err = nvs_flash_init();
+    if (err != ESP_OK) {
+        printf("Failed to initialize NVS\n");
+        blink_led(5, 100); // Blink 5 times to indicate failure
+        return;
+    }
+    
+    // Load and store calibration values
+    err = load_or_store_bias(); // call the function to load or store bias values
+
+    if (err == ESP_OK) {
+        // Sucess
+        printf("Bias values loaded successfully:\n");
+        printf("IMU BIAS VALUES:\n");
+        printf("Accel Bias: X=%.3f, Y=%.3f, Z=%.3f\n",
+               accel_bias[0], accel_bias[1], accel_bias[2]);
+        printf("Gyro  Bias: X=%.3f, Y=%.3f, Z=%.3f\n",
+               gyro_bias[0], gyro_bias[1], gyro_bias[2]);
+
+        // Blink 3 times to indicate success
+        blink_led(3, 200);
+    } else {
+        printf("Error loading bias values: %s\n", esp_err_to_name(err));
+        blink_led(5, 100); // Blink 5 times to indicate failure
+    }
 }
